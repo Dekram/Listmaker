1bc4e6478f3664e97cf286bf81827e3b25383fa0
Dekram <madxtreme@gmail.com>
2023-05-30 23:36:17 +0300

    Add fragment, recyclerview and viewbinding.

---
+++ app/src/main/java/com/dekram/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.dekram.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.dekram.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) : RecyclerView.ViewHolder(binding.root) {
+}
--- app/src/main/java/com/dekram/listmaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/dekram/listmaker/ui/main/MainFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dekram.listmaker.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dekram.listmaker.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -19,14 +22,18 @@ class MainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+        return binding.root
     }
 
 }
